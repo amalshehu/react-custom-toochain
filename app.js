51b41a2583067a2d058b035c72b5e28ff38fa195
Amal Shehu <amalshehu@ashmxmz-titan.local>
2019-11-05 16:16:13 +0530

    Declare useState variable

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
+var _React = React,
+    useState = _React.useState;
 
+console.log('state', useState);
 ReactDOM.render(React.createElement(
   'div',
   null,
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-
+const { useState } = React;
+console.log('state', useState);
 ReactDOM.render(<div>hello world</div>, document.getElementById('root'));
