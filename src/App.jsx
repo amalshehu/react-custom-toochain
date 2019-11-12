0b7977505ff5d974d7e549fcc2e2e4fc14c105fb
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-11-12 18:14:24 +0530

    Added contentEditable

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import 'bulma/css/bulma.css'
-import MarkDown from './MarkDown';
+import showdown from 'showdown'
+import ContentEditable from 'react-contenteditable'
 
-function App() {
+function App () {
   const [cards, setCard] = useState([])
-  const [question, setQuestion] = useState('')
+  const [question, setQuestion] = useState()
   const [answer, setAnswer] = useState('')
-  const [markDown, setMark] = useState('')
+  // const contentEditable = React.createRef()
 
   const textarea = {
-      padding:'20px 100px 20px 100px',
-      margin: '0px'
-  } 
-
-  const container = {
-      marginTop: '60px',
-      display: 'grid',
-      gridGap: '30px',
-      justifyContent: 'center'
+    padding: '20px 100px 20px 100px',
+    margin: '0px'
   }
 
+  const questionBox = {
+    border: '1px solid black',
+    padding: '20px'
+  }
 
-  const handleQuestion = event => setQuestion(event.target.value)
-  
-  const handleAnswer = event => setAnswer(event.target.value)
+  const container = {
+    marginTop: '60px',
+    display: 'grid',
+    gridGap: '30px',
+    justifyContent: 'center'
+  }
 
-  function handleSubmit(event) {
-      event.preventDefault()
-      const card = {
-          id:Date.now(),
-          ques: question,
-          ans:  answer
-      }
-      setCard([card, ...cards])
-      setMark(card.ques)
-      setAnswer('')
-      setQuestion('')
+  const handleQuestion = e => {
+    console.log('onChange Event')
+    setQuestion(e.target.innerText) 
+  }
+
+  const handleAnswer = e => setAnswer(e.target.value)
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    const card = {
+      id: Date.now(),
+      ques: question,
+      ans: answer
+    }
+    setCard([card, ...cards])
+    setMark(card.ques)
+    setAnswer('')
+    setQuestion('')
+  }
+
+  function createMarkUp () {
+    console.log('Question', question)
+    const converter = new showdown.Converter()
+    const html = converter.makeHtml(question)
+    console.log(html)
+    return html
   }
 
   return (
-      <div>
-      <form onSubmit={() => handleSubmit(event)}>
-          <div className="field" style={container}>
-              <div className="control">
-                   <textarea className="textarea is-danger has-fixed-size"
-                    placeholder="Enter the Question"
-                    value={question}
-                     onChange={() => handleQuestion(event)}
-                     style ={textarea}
-                    ></textarea>
-              </div>
-               <div className="control">
-                  <textarea className="textarea is-focused has-fixed-size" 
-                   placeholder="Enter the Answer"
-                   value={answer} onChange={() => handleAnswer(event) }
-                   style={textarea}
-                   ></textarea>
-               </div>
+    <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <div className='field' style={container}>
+        <ContentEditable
+         style={questionBox}
+              // innerRef={contentEditable}
+              html={createMarkUp()} // innerHTML of the editable div
+              disabled={false}       // use true to disable editing
+              onKeyUp={e => handleQuestion(e)} // handle innerHTML change
+            />
+
+          <div className='control'>
+            <textarea
+              className='textarea is-focused has-fixed-size'
+              placeholder='Enter the Answer'
+              value={answer} onChange={e => handleAnswer(e)}
+              style={textarea}
+            />
           </div>
-          <div className="has-text-centered">
-              <button className="button is-success is-rounded is-center" >Save</button>
-          </div>
+        </div>
+        <div className='has-text-centered'>
+          <button className='button is-success is-rounded is-center'>Save</button>
+        </div>
       </form>
-     <MarkDown props={markDown} />
-</div>
+    </div>
   )
 }
 
-export default App;
+export default App
